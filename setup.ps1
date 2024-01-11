e0c8882eec466d3d2837c726c12ba0b23b00fa79
Gonios <hostur2@gmail.com>
2024-01-11 19:17:20 +0100

    Finished setup script

--- setup.ps1
+++ setup.ps1
@@ -51,7 +51,6 @@ Write-Host "Starting setup..." -ForegroundColor Green
 Write-Host ""
 
 # Replace placeholders in files
-# Static definitions string [ "{PROJECT_NAME}_Unity", "{PROJECT_NAME}_FMOD", ... ]
 $fileNames = @(
     @("Template_FMOD/Template_FMOD.fspro", "${projectName}_FMOD.fspro"),
     @("Template_Unity/Template_Unity.sln", "${projectName}_Unity.sln"),
@@ -60,28 +59,62 @@ $fileNames = @(
     @("Template_FMOD", "${projectName}_FMOD")
 );
 
-# iterate over all files and rename them
+# Iterate over all files and rename them
 foreach ($fileName in $fileNames) {
     $oldName = $fileName[0]
     $newName = $fileName[1]
-    Write-Host "Renaming $oldName to $newName"
-    Rename-Item -Path $oldName -NewName $newName
+    if (Test-Path $oldName) 
+    {
+        if (Test-Path $newName) 
+        {
+            Remove-Item $newName -Recurse -Force
+        }
+        Write-Host "Renaming $oldName to $newName"
+        Rename-Item -Path $oldName -NewName $newName
+    }
+    else {
+        Write-Host "[Skipped] File $oldName not found" -ForegroundColor DarkGray
+    }
 }
 
 # Replace placeholders in files
-# Dynamic definitions string [ [ "{PROJECT_NAME}_Unity.jsproj", "Template_Unity", "{PROJECT_NAME}" ], ... ]
-# $dynamicFileNames = @(
-#     [string[]]@("${projectName}_Unity/${projectName}_Unity.jsproj", "productName: Template_Unity", "productName: ${projectName}")
-#     [string[]]@("${projectName}_Unity/${projectName}_Unity.jsproj", "productName: Template_Unity", "metroPackageName: ${projectName}")
-#     [string[]]@("${projectName}_Unity/${projectName}_Unity.jsproj", "productName: Template_Unity", "metroApplicationDescription: ${projectName}")
-#     [string[]]@("${projectName}_FMOD/Workspace.xml", "../Template_Unity", "../${projectName}_Unity")
-# );
+$dynamicFileNames = @(
+    @("${projectName}_Unity/${projectName}_Unity.jsproj", "productName: Template_Unity", "productName: ${projectName}"),
+    @("${projectName}_Unity/${projectName}_Unity.jsproj", "metroPackageName: Template_Unity", "metroPackageName: ${projectName}"),
+    @("${projectName}_Unity/${projectName}_Unity.jsproj", "metroApplicationDescription: Template_Unity", "metroApplicationDescription: ${projectName}"),
+    @("${projectName}_FMOD/Metadata/Workspace.xml", "../Template_Unity", "../${projectName}_Unity")
+);
 
-# # iterate over all files replace all placeholders
-# foreach ($fileName in $dynamicFileNames) {
-#     $filePath = $fileName[0]
-#     $oldString = $fileName[1]
-#     $newString = $fileName[2]
-#     Write-Host "Replacing $oldString with $newString in $filePath"
-#     (Get-Content $filePath) | Foreach-Object { $_ -replace $oldString, $newString } | Set-Content $filePath
-# }
+# Iterate over all files replace all placeholders
+foreach ($fileName in $dynamicFileNames) {
+    $filePath = $fileName[0]
+    $oldString = $fileName[1]
+    $newString = $fileName[2]
+    if (Test-Path $filePath) 
+    {
+        Write-Host "Replaced $oldString with $newString in $filePath"
+        (Get-Content $filePath) | Foreach-Object { $_ -replace $oldString, $newString } | Set-Content $filePath
+    }
+    else {
+        Write-Host "[Skipped] File $filePath not found" -ForegroundColor DarkGray
+    }
+}
+
+# Clean up template files
+if ($cleanUp -eq "Y" -or $cleanUp -eq "y") {
+    $cleamUpFiles = @("setup.ps1", "setup.sh");
+    foreach ($fileName in $cleamUpFiles) {
+        if (Test-Path $fileName) 
+        {
+            Remove-Item $fileName -Force
+        }
+    }
+    Write-Host ""
+    Write-Host "Cleaned up template files"
+    Write-Host ""
+}
+
+# Write Finish Message
+Write-Host ""
+Write-Host "Setup finished, Happy coding!" -ForegroundColor Green
+Write-Host ""
